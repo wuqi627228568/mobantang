0ad798b6d27f15c12f8900936aa5a2dcfa68d2cb
wuqi627228568 <wuqi95190@163.com>
2018-03-01 21:09:13 +0800

    数据上传未完成

--- my-project/routes/index.js
+++ my-project/routes/index.js
@@ -8,38 +8,42 @@ router.get('/', function(req, res) {
 router.get('/shouye', function(req, res) {
   res.render('shouye', { title: '' });
 });
-
-router.post("/api/login",function(req,res){
-	var username=req.body.username;
-	var psw=req.body.psw;
-	UserModel.find({username:username,psw:psw},function(err,docs){
-		if(!err&&docs.length>0){
-			console.log("登录成功");
-			res.send("登录成功")
-		}else{
-			console.log("登录失败，请检查您的用户名或者密码");
-			res.send("登录失败，请检查您的用户名或者密码")
-		}
-	})
-})
-//router.post("/api/login", function(req, res) {
-//	var username = req.body.username;
-//	var psw = md5(req.body.psw);
-//
-//	var result = {
-//		status: 1,
-//		message: "登录成功"
-//	}
-//	UserModel.find({username: username, psw: psw}, function(err, docs){
-//		if(!err && docs.length > 0) {
+router.get('/sptj', function(req, res) {
+  res.render('sptj', { title: '' });
+});
+router.get('/splb', function(req, res) {
+  res.render('splb', { title: '' });
+});
+//router.post("/api/login",function(req,res){
+//	var username=req.body.username;
+//	var psw=req.body.psw;
+//	UserModel.find({username:username,psw:psw},function(err,docs){
+//		if(!err&&docs.length>0){
 //			console.log("登录成功");
-//			res.send(result);
-//		} else {
+//			res.send("登录成功")
+//		}else{
 //			console.log("登录失败，请检查您的用户名或者密码");
-//			result.status = -109;
-//			result.message = "登录失败，请检查您的用户名或者密码"
-//			res.send(result);
+//			res.send("登录失败，请检查您的用户名或者密码")
 //		}
 //	})
 //})
+router.post("/api/login", function(req, res) {
+	var username = req.body.username;
+	var psw = req.body.psw;
+	var result = {
+		status: 1,
+		message: "登录成功"
+	}
+	UserModel.find({username: username, psw: psw}, function(err, docs){
+		if(!err && docs.length > 0) {
+			console.log("登录成功");
+			res.send(result);
+		} else {
+			console.log("登录失败，请检查您的用户名或者密码");
+			result.status = -109;
+			result.mssagee = "登录失败，请检查您的用户名或者密码"
+			res.send(result);
+		}
+	})
+})
 module.exports = router;
